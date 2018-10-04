232fe1fa7e48d0b0c8f8085324ebe327054b2f0b
MINSK\Siarhei_Mokin <limonad-vafel@list.ru>
2018-10-04 18:09:32 +0300

    Added kirsch filter.

--- ServerSide/GraduationProject/GraduationProjectImageHandler/ImageHandler.cs
+++ ServerSide/GraduationProject/GraduationProjectImageHandler/ImageHandler.cs
@@ -10,14 +10,15 @@ using System.Globalization;
 using System.Linq;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
+using System.Runtime.InteropServices;
 
 namespace GraduationProjectImageHandler
 {
     // Process images class.
     public class ImageHandler : IImageHandler
     {
-        private Tuple<string, string> _answerVariants = new Tuple<string, string>("YES", "NO");
-        private Dictionary<int, string> _answers = new Dictionary<int, string>();
+        private readonly Tuple<string, string> _answerVariants = new Tuple<string, string>("YES", "NO");
+        private readonly Dictionary<int, string> _answers = new Dictionary<int, string>();
         private string[] _questions;
 
         private string _savedImageName;
@@ -103,8 +104,8 @@ namespace GraduationProjectImageHandler
                 var i = 1;
                 foreach (var c in AnswerCoordinates.Coordinates)
                 {
-                    var blackPixelLeftCount = SearchCountOfBlackPixelsByCoordinates(img, c.Value[0], c.Value[1], c.Value[2], c.Value[3]);
-                    var blackPixelRightCount = SearchCountOfBlackPixelsByCoordinates(img, c.Value[4], c.Value[5], c.Value[6], c.Value[7]);
+                    var blackPixelLeftCount = SearchCountOfWhitePixelsByCoordinates(img, c.Value[0], c.Value[1], c.Value[2], c.Value[3]);
+                    var blackPixelRightCount = SearchCountOfWhitePixelsByCoordinates(img, c.Value[4], c.Value[5], c.Value[6], c.Value[7]);
 
                     _answers.Add(i++, blackPixelLeftCount > blackPixelRightCount ? _answerVariants.Item1 : _answerVariants.Item2);
                 }
@@ -112,23 +113,6 @@ namespace GraduationProjectImageHandler
 
             File.Delete(_savedImageName);
             File.Delete(_savedBlackWhiteImageName);
-        }
-
-        private int SearchCountOfBlackPixelsByCoordinates(Bitmap img, int startX, int startY, int endX, int endY)
-        {
-            var blackPixelCount = 0;
-            for (var x = startX; x < endX; x++)
-            {
-                for (var y = startY; y < endY; y++)
-                {
-                    var color = img.GetPixel(x, y);
-                    if ((color.R + color.G + color.B) < 5)
-                    {
-                        blackPixelCount++;
-                    }
-                }
-            }
-            return blackPixelCount;
         }
 
         private void SaveImage()
@@ -143,21 +127,152 @@ namespace GraduationProjectImageHandler
 
         private void SaveGrayScaleImage()
         {
-            using (var image = new Bitmap(_savedImageName))
+            using (var img = new Bitmap(_savedImageName))
+                using (var resizedImg = ResizeBitmap(img, new Size { Width = 1080, Height = 1397 }))
+                    using (var kirschImg = KirschFilter(resizedImg))
+                        using (var bw = kirschImg.Clone(new Rectangle(0, 0, kirschImg.Width, kirschImg.Height), PixelFormat.Format1bppIndexed))
+                            bw.Save(_savedBlackWhiteImageName);
+        }
+
+        private static int SearchCountOfWhitePixelsByCoordinates(Bitmap img, int startX, int startY, int endX, int endY)
+        {
+            var whitePixelCount = 0;
+            for (var x = startX; x < endX; x++)
             {
-                using (var resImg = ResizeBitmap(image, new Size { Width = 1080, Height = 1397 }))
+                for (var y = startY; y < endY; y++)
                 {
-                    using (var bw = resImg.Clone(new Rectangle(0, 0, resImg.Width, resImg.Height), PixelFormat.Format1bppIndexed))
+                    var color = img.GetPixel(x, y);
+                    if ((color.R + color.G + color.B) > 5)
                     {
-                        bw.Save(_savedBlackWhiteImageName);
+                        whitePixelCount++;
                     }
                 }
             }
+            return whitePixelCount;
         }
 
-        private Bitmap ResizeBitmap(Image imgToResize, Size size)
+        private static Bitmap ResizeBitmap(Image imgToResize, Size size)
         {
             return new Bitmap(imgToResize, size);
+        }
+
+        private static Bitmap KirschFilter(Bitmap sourceBitmap, bool grayscale = true)
+        {
+            var resultBitmap = ConvolutionFilter(sourceBitmap, Kirsch3X3Horizontal, Kirsch3X3Vertical, grayscale);
+            
+            return resultBitmap;
+        }
+
+        private static double[,] Kirsch3X3Horizontal => new double[,]
+        {
+            {  5,  5,  5 },
+            { -3,  0, -3 },
+            { -3, -3, -3 },
+        };
+
+        private static double[,] Kirsch3X3Vertical => new double[,]
+        {
+            {  5, -3, -3, },
+            {  5,  0, -3, },
+            {  5, -3, -3, },
+        };
+
+        private static Bitmap ConvolutionFilter(Bitmap sourceBitmap, double[,] xFilterMatrix, double[,] yFilterMatrix, bool grayscale = false)
+        {
+            var sourceData =
+                sourceBitmap.LockBits(new Rectangle(0, 0,
+                        sourceBitmap.Width, sourceBitmap.Height),
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format32bppArgb);
+
+            var pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            var resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+            sourceBitmap.UnlockBits(sourceData);
+
+            if (grayscale)
+            {
+                for (var k = 0; k < pixelBuffer.Length; k += 4)
+                {
+                    var rgb = pixelBuffer[k] * 0.11f;
+                    rgb += pixelBuffer[k + 1] * 0.59f;
+                    rgb += pixelBuffer[k + 2] * 0.3f;
+
+
+                    pixelBuffer[k] = (byte)rgb;
+                    pixelBuffer[k + 1] = pixelBuffer[k];
+                    pixelBuffer[k + 2] = pixelBuffer[k];
+                    pixelBuffer[k + 3] = 255;
+                }
+            }
+
+            const int filterOffset = 1;
+
+            for (var offsetY = filterOffset;offsetY < sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (var offsetX = filterOffset; offsetX < sourceBitmap.Width - filterOffset; offsetX++)
+                {
+                    double greenX;
+                    double redX;
+                    var blueX = greenX = redX = 0;
+                    double greenY;
+                    double redY;
+                    var blueY = greenY = redY = 0;
+                    
+                    var byteOffset = offsetY * sourceData.Stride + offsetX * 4;
+                    
+                    for (var filterY = -filterOffset; filterY <= filterOffset; filterY++)
+                    {
+                        for (var filterX = -filterOffset; filterX <= filterOffset; filterX++)
+                        {
+                            var calcOffset = byteOffset + (filterX * 4) + (filterY * sourceData.Stride);
+                            
+                            blueX += pixelBuffer[calcOffset] * xFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                            
+                            greenX += pixelBuffer[calcOffset + 1] * xFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                            
+                            redX += (pixelBuffer[calcOffset + 2]) * xFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                            
+                            blueY += (pixelBuffer[calcOffset]) * yFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                            
+                            greenY += (pixelBuffer[calcOffset + 1]) * yFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                            
+                            redY += (pixelBuffer[calcOffset + 2]) * yFilterMatrix[filterY + filterOffset, filterX + filterOffset];
+                        }
+                    }
+                    
+                    var blueTotal = Math.Sqrt((blueX * blueX) + (blueY * blueY));
+                    
+                    var greenTotal = Math.Sqrt((greenX * greenX) + (greenY * greenY));
+                    
+                    var redTotal = Math.Sqrt((redX * redX) + (redY * redY));
+
+                    blueTotal = blueTotal > 255 ? 255 : blueTotal < 0 ? 0 : blueTotal;
+
+                    greenTotal = greenTotal > 255 ? 255 : greenTotal < 0 ? 0 : greenTotal;
+
+                    redTotal = redTotal > 255 ? 255 : redTotal < 0 ? 0 : redTotal;
+                    
+                    resultBuffer[byteOffset] = (byte)(blueTotal);
+                    resultBuffer[byteOffset + 1] = (byte)(greenTotal);
+                    resultBuffer[byteOffset + 2] = (byte)(redTotal);
+                    resultBuffer[byteOffset + 3] = 255;
+                }
+            }
+            
+            var resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            
+            var resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+            
+            return resultBitmap;
         }
     }
 }
